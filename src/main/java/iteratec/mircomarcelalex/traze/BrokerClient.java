95988d5f47bb6fd476a6222463288d5bcfecb5c8
5heinzel <5heinzel@informatik.uni-hamburg.de>
2018-03-16 14:55:41 +0100

    trying to join

--- src/main/java/iteratec/mircomarcelalex/traze/BrokerClient.java
+++ src/main/java/iteratec/mircomarcelalex/traze/BrokerClient.java
@@ -1,19 +1,26 @@
 package iteratec.mircomarcelalex.traze;
 
 import org.eclipse.paho.client.mqttv3.MqttClient;
-import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.json.JSONObject;
+
+import java.util.UUID;
 
 class BrokerClient {
 
-	private SimpleMqttCallBack ourCallback = new SimpleMqttCallBack();
-	
+    private MqttClient client;
+    private SimpleMqttCallBack ourCallback = new SimpleMqttCallBack();
+
     BrokerClient() {
         try {
-            MqttClient client = new MqttClient("tcp://traze.iteratec.de:1883", MqttClient.generateClientId());
+            client = new MqttClient("tcp://traze.iteratec.de:1883", MqttClient.generateClientId());
             client.setCallback(ourCallback);
             client.connect();
             client.subscribe("traze/1/grid");
+
+            String name = join("Mirco");
+            client.subscribe("traze/1/player/" + name);
+
 //            client.subscribe("traze/1/players");
 //            client.subscribe("traze/1/ticker");
         } catch (Exception e) {
@@ -21,11 +28,24 @@ class BrokerClient {
         }
     }
 
-    public void sendMessage(String message, String topic) {
+    public String join(String nickname) {
+        String topic = "traze/1/join";
 
+        String uniqueName = "" + UUID.randomUUID();
+        JSONObject joiningPlayer = new JSONObject("{\"name\": \"" + nickname + "\",\"mqttClientName\": \"" + uniqueName + "\"}");
+        System.out.println(joiningPlayer.toString());
+
+        MqttMessage message = new MqttMessage();
+        message.setPayload(joiningPlayer.toString().getBytes());
+        try {
+            client.publish(topic, message);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return uniqueName;
     }
-    
-    public SimpleMqttCallBack getCallBack(){
-    	return ourCallback;
+
+    public SimpleMqttCallBack getCallBack() {
+        return ourCallback;
     }
 }
