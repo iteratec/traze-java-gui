1934d4a8362df1953594f1e3432b2cffa2aa566c
5heinzel <5heinzel@informatik.uni-hamburg.de>
2018-03-16 10:41:39 +0100

    fix?

--- src/main/java/iteratec/mircomarcelalex/traze/BrokerClient.java
+++ src/main/java/iteratec/mircomarcelalex/traze/BrokerClient.java
@@ -7,9 +7,8 @@ import org.eclipse.paho.client.mqttv3.MqttMessage;
 class BrokerClient {
 
     BrokerClient() {
-        MqttClient client = null;
         try {
-            client = new MqttClient("traze.iteratec.de:1883", MqttClient.generateClientId());
+            MqttClient client = new MqttClient("traze.iteratec.de:1883", MqttClient.generateClientId());
             client.connect();
             MqttMessage message = new MqttMessage();
             message.setPayload("Hello world from Java".getBytes());
