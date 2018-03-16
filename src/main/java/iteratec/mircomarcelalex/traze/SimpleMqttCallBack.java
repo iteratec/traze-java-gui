6d2fa5a733d7582f937e1eec3da17013d519b4e4
5heinzel <5heinzel@informatik.uni-hamburg.de>
2018-03-16 16:22:55 +0100

    well, we dont need those playerz

--- src/main/java/iteratec/mircomarcelalex/traze/SimpleMqttCallBack.java
+++ src/main/java/iteratec/mircomarcelalex/traze/SimpleMqttCallBack.java
@@ -19,9 +19,10 @@ public class SimpleMqttCallBack implements MqttCallback {
             TrazeClient.steer();
         } else if (topic.equals("traze/1/player/" + BrokerClient.generatedClientId)) {
             TrazeClient.initPlayer(new String(mqttMessage.getPayload()));
-        }else if (topic.equals("traze/1/players" )) {
-            TrazeClient.setPlayers(new String(mqttMessage.getPayload()));
         }
+//        else if (topic.equals("traze/1/players" )) {
+//            TrazeClient.setPlayers(new String(mqttMessage.getPayload()));
+//        }
     }
 
     public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
